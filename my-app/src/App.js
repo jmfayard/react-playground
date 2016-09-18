ad5b50f2c23d90ca3d9585a5510431a17c3a43c4
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2016-09-18 15:54:45 +0200

    bootstrap-react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './Button';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" role="presentation"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+import { Navbar, Jumbotron, Button, Grid } from 'react-bootstrap';
 
+var root = React.DOM.ul({className: 'my-list'},
+    React.DOM.li(null, 'Text Content')
+);
+
+
+
+const Brand = (props) => (
+    <Navbar.Header>
+        <Navbar.Brand>
+            <a href={props.url}>{props.text}</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+    </Navbar.Header>
+);
+
+
+const Input = (props) => (
+    <div className="form-group">
+        <label >{props.label}</label>
+        <input type={props.type} placeholder="text goes here" onChange={props.update} >
+
+        </input>
+    </div>
+);
+
+const App = () => (
+    <div>
+        <Navbar inverse fixedTop>
+            <Grid>
+                <Brand text="My-App" url="http://google.com">
+                </Brand>
+            </Grid>
+        </Navbar>
+        <Jumbotron>
+            <Grid>
+                <h1>Welcome to React</h1>
+                <p>
+                    <Button
+                        bsStyle="success"
+                        bsSize="large"
+                        href="http://react-bootstrap.github.io/components.html"
+                        target="_blank">
+                        View React Bootstrap Docs
+                    </Button>
                 </p>
-            </div>
-        );
-    }
-}
+            </Grid>
+        </Jumbotron>
+        <div>
+            <Input type="text" label="First Name"></Input>
+            <Input type="number" label="Age"></Input>
+        </div>
+    </div>
+);
 
 
 export default App;
